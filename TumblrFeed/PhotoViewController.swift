bae95e50d63015886adadd0786bfae304b1402a3
Isaac <isaac@admins-MacBook-Pro.local>
2018-09-19 00:43:17 -0700

    lab 2 finished

--- TumblrFeed/PhotoViewController.swift
+++ TumblrFeed/PhotoViewController.swift
@@ -13,36 +13,20 @@ class PhotoViewController: UIViewController,UITableViewDataSource, UITableViewDe
     
     var posts: [[String: Any]] = []
     @IBOutlet weak var PhotoTableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         PhotoTableView.insertSubview(refreshControl, at: 0)
-
+        
         PhotoTableView.delegate = self
         PhotoTableView.dataSource = self
-
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data,
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print(dataDictionary)
-                // Get the dictionary from the response key
-                let responseDictionary = dataDictionary["response"] as! [String: Any]
-                // Store the returned array of dictionaries in our posts property
-                self.posts = responseDictionary["posts"] as! [[String: Any]]
-                
-                self.PhotoTableView.reloadData()
-            }
-        }
-        task.resume()
+        
+        refreshControlAction(refreshControl)
     }
+    
     
     @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
         
@@ -78,35 +62,37 @@ class PhotoViewController: UIViewController,UITableViewDataSource, UITableViewDe
         let post = posts[indexPath.row]
         if let photos = post["photos"] as? [[String: Any]] {
             let photo = photos[0]
-        
+            
             let originalSize = photo["original_size"] as! [String: Any]
-         
+            
             let urlString = originalSize["url"] as! String
             
             let url = URL(string: urlString)
             cell.photoImageView.af_setImage(withURL: url!)
-         
+            
         }
-       // cell.textLabel?.text = "This is row \(indexPath.row)"
         
         return cell
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = PhotoTableView.indexPath(for: cell){
+            let post = posts[indexPath.row]
+            let vc = segue.destination as! PhotoDetailsViewController
+            vc.post = post
+        }
+    
 
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
